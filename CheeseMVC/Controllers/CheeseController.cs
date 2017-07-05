24c5c284163beeeb8987098a34f92a81d457ebb7
cperceful <cpercef1@gmail.com>
2017-07-05 11:20:06 -0500

    Finished class 3 prep videos

--- CheeseMVC/Controllers/CheeseController.cs
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -10,10 +10,33 @@ namespace CheeseMVC.Controllers
 {
     public class CheeseController : Controller
     {
+        Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+
         // GET: /<controller>/
         public IActionResult Index()
         {
+           
+            ViewBag.cheeses = Cheeses; //this passses to the view, allowing it to be accessed
+
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name, string description)
+        {
+            Cheeses.Add(name, description);
+
+            return Redirect("/cheese");
+        }
+
+        
     }
+
+
 }
