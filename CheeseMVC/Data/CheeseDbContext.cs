1b34635e3a3eddbbe1726b5bc827bfe85dd91fcc
cperceful <cpercef1@gmail.com>
2017-07-19 17:15:18 -0500

    Created CheeseCategory class, added DbSet to CheeseDbContext

--- CheeseMVC/Data/CheeseDbContext.cs
+++ CheeseMVC/Data/CheeseDbContext.cs
@@ -11,6 +11,7 @@ namespace CheeseMVC.Data
     {
 
         public DbSet<Cheese> Cheeses { get; set; }
+        public DbSet<CheeseCategory> Categories { get; set; }
 
         public CheeseDbContext(DbContextOptions<CheeseDbContext> options) : base(options)
         {
