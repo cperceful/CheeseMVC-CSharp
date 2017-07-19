a9188ae785310c01e092ff3e421086f2043ffe70
cperceful <cpercef1@gmail.com>
2017-07-19 15:26:09 -0500

    Changed Startup.cs to incorporate databse

--- CheeseMVC/Startup.cs
+++ CheeseMVC/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using CheeseMVC.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CheeseMVC
 {
@@ -29,6 +31,9 @@ namespace CheeseMVC
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddDbContext<CheeseDbContext>(options =>
+    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
